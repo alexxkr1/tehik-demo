302c5027a38feae8ff7cb0b98a9704f3e72709ea
Alex Kren <alex.kren@flowit.ee>
2025-11-10 15:20:56 +0200

    Init

---
+++ tehik-api/src/main/java/com/example/tehik/TehikApplication.java
@@ -0,0 +1,13 @@
+package com.example.tehik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehikApplication.class, args);
+	}
+
+}
---
+++ tehik-api/src/test/java/com/example/tehik/TehikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tehik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TehikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
