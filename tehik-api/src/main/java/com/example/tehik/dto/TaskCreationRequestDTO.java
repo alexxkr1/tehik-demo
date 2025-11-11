28fa4d2ccf9a698aefd90a725e870b4a7435aaea
Alex Kren <alex.kren@flowit.ee>
2025-11-11 16:31:12 +0200

    Update dto validation message that suits better with frontend

--- tehik-api/src/main/java/com/example/tehik/dto/TaskCreationRequestDTO.java
+++ tehik-api/src/main/java/com/example/tehik/dto/TaskCreationRequestDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record TaskCreationRequestDTO(
-        @NotNull(message = "Original value must be present")
-        @Min(value = 1, message = "Original value must be positive")
+        @NotNull(message = "Task value must be present")
+        @Min(value = 1, message = "Task value must be positive")
         Integer originalValue
 ) { }
