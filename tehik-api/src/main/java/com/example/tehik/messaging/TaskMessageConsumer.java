9eb03dbb76a7e359c0def91f0fb6296e39ab2a4b
Alex Kren <alex.kren@flowit.ee>
2025-11-12 15:20:07 +0200

    Add 10 second wait for better debugging websocket

--- tehik-api/src/main/java/com/example/tehik/messaging/TaskMessageConsumer.java
+++ tehik-api/src/main/java/com/example/tehik/messaging/TaskMessageConsumer.java
@@ -1,8 +1,11 @@
 package com.example.tehik.messaging;
 
+import com.example.tehik.dto.TaskResponseDTO;
 import com.example.tehik.entity.Task;
 import com.example.tehik.enums.TaskStatus;
+import com.example.tehik.mappers.TaskMapper;
 import com.example.tehik.repository.TaskRepository;
+import com.example.tehik.service.TaskStatusUpdateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Component;
 public class TaskMessageConsumer {
 
     private final TaskRepository taskRepository;
+    private  final TaskStatusUpdateService taskStatusUpdateService;
+    private final TaskMapper taskMapper;
 
     @RabbitListener(queues = {"${app.rabbitmq.queue}"})
     public void receiveMessage(Long taskId) {
@@ -22,12 +27,25 @@ public class TaskMessageConsumer {
         Task task = taskRepository.findById(taskId)
                 .orElseThrow(() -> new RuntimeException("Task not found with ID: " + taskId));
 
+
+        // Wait 10 seconds for development purposes to test working with websocket
+        try {
+            Thread.sleep(10000); // 10 seconds
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.warn("Task processing sleep interrupted", e);
+        }
+
         Integer result = task.getOriginalValue() * 5;
 
         task.setResultValue(result);
         task.setStatus(TaskStatus.DONE);
 
         taskRepository.save(task);
+
+        TaskResponseDTO taskDto = taskMapper.toResponseDTO(task);
+
+        taskStatusUpdateService.sendUpdate(taskDto);
 
         log.info("Task ID {} processed successfully. Result: {}", taskId, result);
     }
